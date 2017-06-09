221801e4a8fab499dd4ec2d0b05d87122e88d005
Sudhanshu Ranjan <sud.ranjan999@gmail.com>
2017-06-09 18:24:48 +0530

    Add files via upload

--- resources.js
+++ resources.js
@@ -1,3 +1,29 @@
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
 $(document).ready(function(){
     $("#basic-info").click(function(){
         $(".basic-info").show();
@@ -7,7 +33,7 @@ $(document).ready(function(){
         $(".location").hide();
         $(".necessary").hide();
         $(".counselling").hide();
-
+        $(".mentor").hide();
     });
 
     $("#documents").click(function(){
@@ -18,6 +44,7 @@ $(document).ready(function(){
         $(".location").hide();
         $(".necessary").hide();
         $(".counselling").hide();
+        $(".mentor").hide();
     });
     $("#facilities").click(function(){
         $(".basic-info").hide();
@@ -27,6 +54,7 @@ $(document).ready(function(){
         $(".location").hide();
         $(".necessary").hide();
         $(".counselling").hide();
+        $(".mentor").hide();
     });
     $("#registration").click(function(){
         $(".basic-info").hide();
@@ -36,6 +64,7 @@ $(document).ready(function(){
         $(".location").hide();
         $(".necessary").hide();
         $(".counselling").hide();
+        $(".mentor").hide();
     });
     $("#location").click(function(){
         $(".basic-info").hide();
@@ -45,6 +74,7 @@ $(document).ready(function(){
         $(".location").show();
         $(".necessary").hide();
         $(".counselling").hide();
+        $(".mentor").hide();
     });
     $("#necessary").click(function(){
         $(".basic-info").hide();
@@ -54,6 +84,7 @@ $(document).ready(function(){
         $(".location").hide();
         $(".necessary").show();
         $(".counselling").hide();
+        $(".mentor").hide();
     });
     $("#counselling").click(function(){
         $(".basic-info").hide();
@@ -63,6 +94,17 @@ $(document).ready(function(){
         $(".location").hide();
         $(".necessary").hide();
         $(".counselling").show();
+        $(".mentor").hide();
+    });
+    $("#mentor").click(function(){
+        $(".basic-info").hide();
+        $(".documents").hide();
+        $(".facilities").hide();
+        $(".registration").hide();
+        $(".location").hide();
+        $(".necessary").hide();
+        $(".counselling").hide();
+        $(".mentor").show();
     });
 
 });
